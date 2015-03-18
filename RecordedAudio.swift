6c0c085219b90500051604e9d1182c316bf059a1
Victor Guerra <vguerra@gmail.com>
2015-03-19 00:49:56 +0100

    Documenting RecordedAudio class

--- RecordedAudio.swift
+++ RecordedAudio.swift
@@ -8,10 +8,28 @@
 
 import Foundation
 
+/// MARK: Printable
+
+/*! 
+    Represents a file recorded
+*/
+
 class RecordedAudio: NSObject {
+    
+    /*! 
+        Audio file URL
+    */
     var filePathUrl: NSURL
+    /*! 
+        Audio file title
+    */
     var title: String
     
+    /*! 
+        Initialises an object with a given path URL and title
+        @param filePathURL A NSURL object indicating the path of the audio file
+        @param titlea The audio file title
+    */
     init(filePathUrl:NSURL, title:String) {
         self.filePathUrl = filePathUrl
         self.title = title
